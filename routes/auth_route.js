e3249f7c1241859ba37350a783ec629096adde45
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-26 02:58:28 +0700

    Create Logout API

--- routes/auth_route.js
+++ routes/auth_route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signUp, signIn, google } from '../controllers/auth_controller.js';
+import { signUp, signIn, google, signOut } from '../controllers/auth_controller.js';
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.post("/google", google);
+router.get("/signout", signOut);
 
 export default router;
