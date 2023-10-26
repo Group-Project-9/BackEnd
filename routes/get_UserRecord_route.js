1aa1055b064f69d1eb8acecad099e1aafbce63e1
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-26 14:50:25 +0700

    Test2

--- routes/get_UserRecord_route.js
+++ routes/get_UserRecord_route.js
@@ -4,14 +4,14 @@ import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get("/user_record", verifyToken, getUserRecord);
-router.put("/user_update_record/:id", verifyToken, updateUserRecord);
-router.delete("/user_delete_Record", verifyToken, deleteUserRecord);
-router.post("/update_user_info/:id", verifyToken, updateUserInfo);
+// router.get("/user_record", verifyToken, getUserRecord);
+// router.put("/user_update_record/:id", verifyToken, updateUserRecord);
+// router.delete("/user_delete_Record", verifyToken, deleteUserRecord);
+// router.post("/update_user_info/:id", verifyToken, updateUserInfo);
 
-// router.get("/user_record", getUserRecord);
-// router.put("/user_update_record/:id", updateUserRecord);
-// router.delete("/user_delete_Record", deleteUserRecord);
-// router.post("/update_user_info/:id", updateUserInfo);
+router.get("/user_record", getUserRecord);
+router.put("/user_update_record/:id", updateUserRecord);
+router.delete("/user_delete_Record", deleteUserRecord);
+router.post("/update_user_info/:id", updateUserInfo);
 
 export default router;
