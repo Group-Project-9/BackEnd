6d80a2fb8fdd4a8a02e05642247d6e5cf2986719
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-26 14:54:44 +0700

    Test3

--- routes/record_route.js
+++ routes/record_route.js
@@ -4,11 +4,18 @@ import { activityUpdate , addRecord, readRecord , userProfile , updateRecord , d
 import { verifyToken } from '../utils/verifyUser.js';
 
 
-router.post("/add", verifyToken, addRecord);
-router.get("/read", verifyToken, readRecord);
-router.get("/userProfile" , verifyToken, userProfile );
-router.put("/update", verifyToken, updateRecord);
-router.put("/utest", verifyToken, activityUpdate)
-router.delete("/delete", verifyToken, deleteRecord2);
+// router.post("/add", verifyToken, addRecord);
+// router.get("/read", verifyToken, readRecord);
+// router.get("/userProfile" , verifyToken, userProfile );
+// router.put("/update", verifyToken, updateRecord);
+// router.put("/utest", verifyToken, activityUpdate)
+// router.delete("/delete", verifyToken, deleteRecord2);
+
+router.post("/add", addRecord);
+router.get("/read", readRecord);
+router.get("/userProfile" , userProfile );
+router.put("/update", updateRecord);
+router.put("/utest", activityUpdate)
+router.delete("/delete", deleteRecord2);
 
 export default router;
