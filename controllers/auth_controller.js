6cddaf53a1f324c41230dfe702eb57d27bcc39e0
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-26 15:15:37 +0700

    Fix Auth Login

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -31,30 +31,28 @@ export const signUp = async (req, res, next) => {
 
 // Sign in a user
 export const signIn = async(req, res, next) => {
-    // Get the email and password from the request body
-    const { email, password } = req.body;
+    
+  const { email, password } = req.body;
     console.log(req.body)
     
     try {
-        // Find the user
-        const validUser = await User.findOne({ email });
+
+      const validUser = await User.findOne({ email });
         
-        // If the user is not found, return an error
         if (!validUser) { return next(errorHandler(404, "Email not found")); }
         
-        // If the user is found, compare the password
         const validPassword = bcrypt.compareSync(password, validUser.password);
-        // If the password is invalid, return an error
+
         if (!validPassword) { return next(errorHandler(401, "Invalid Password")); }
-        // If the password is valid, create a token
+
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
-        //  Remove the password from the user object
+
         const { password: userPassword, ...others } = validUser._doc;
-        // Return the user and the token
+
         res
             .cookie("access_token", token, { httpOnly: true })
             .status(200)
-            .json(token);
+            .json(others);
         
     } catch (error) {
         next(error);
